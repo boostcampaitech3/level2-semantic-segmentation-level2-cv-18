616f2400ae58ed81a803085986eb630b6f7122c3
ed-kyu <seungkyukim.dev@gmail.com>
2022-05-02 11:38:39 +0900

    remove: ocrnet

--- mmsegmentation/ocrnet/configs/_boost_/train_models/fcn_hr18_512x512_40k_voc12aug.py
+++
@@ -1,7 +0,0 @@
-_base_ = [
-    '../models/fcn_hr18.py',
-    '../datasets/custom_dataset.py',
-    '../default_runtime.py',
-    '../schedules/schedule.py'
-]
-model = dict(decode_head=dict(num_classes=11))
