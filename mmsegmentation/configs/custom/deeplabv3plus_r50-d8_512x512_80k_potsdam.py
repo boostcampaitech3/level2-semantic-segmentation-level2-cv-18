b7307f51e895a668546f230e8e16ba5c0ae13b07
kimkihoon0515 <tega1996@naver.com>
2022-05-06 01:31:19 +0000

    deeplabv3+ with resnet50config file upload

---
+++ mmsegmentation/configs/custom/deeplabv3plus_r50-d8_512x512_80k_potsdam.py
@@ -0,0 +1,244 @@
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+model = dict(
+    type='EncoderDecoder',
+    pretrained='open-mmlab://resnet50_v1c',
+    backbone=dict(
+        type='ResNetV1c',
+        depth=50,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        dilations=(1, 1, 2, 4),
+        strides=(1, 2, 1, 1),
+        norm_cfg=dict(type='SyncBN', requires_grad=True),
+        norm_eval=False,
+        style='pytorch',
+        contract_dilation=True),
+    decode_head=dict(
+        type='DepthwiseSeparableASPPHead',
+        in_channels=2048,
+        in_index=3,
+        channels=512,
+        dilations=(1, 12, 24, 36),
+        c1_in_channels=256,
+        c1_channels=48,
+        dropout_ratio=0.1,
+        num_classes=11,
+        norm_cfg=dict(type='SyncBN', requires_grad=True),
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+    auxiliary_head=dict(
+        type='FCNHead',
+        in_channels=1024,
+        in_index=2,
+        channels=256,
+        num_convs=1,
+        concat_input=False,
+        dropout_ratio=0.1,
+        num_classes=11,
+        norm_cfg=dict(type='SyncBN', requires_grad=True),
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4)),
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole'))
+dataset_type = 'CustomDataset'
+data_root = '/opt/ml/input/mmseg/'
+classes = [
+    'Background', 'General trash', 'Paper', 'Paper pack', 'Metal', 'Glass',
+    'Plastic', 'Styrofoam', 'Plastic bag', 'Battery', 'Clothing'
+]
+palette = [[0, 0, 0], [192, 0, 128], [0, 128, 192], [0, 128, 64], [128, 0, 0],
+           [64, 0, 128], [64, 0, 192], [192, 128, 64], [192, 192, 128],
+           [64, 64, 128], [128, 0, 192]]
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+crop_size = (512, 512)
+train_pipeline = [
+    dict(type='RandomMosaic', prob=0.5, img_scale=(512, 512)),
+    dict(type='Resize', img_scale=(512, 512), ratio_range=(0.5, 2.0)),
+    dict(type='RandomCrop', crop_size=(512, 512), cat_max_ratio=0.75),
+    dict(type='CLAHE'),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PhotoMetricDistortion'),
+    dict(
+        type='Normalize',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        to_rgb=True),
+    dict(type='Pad', size=(512, 512), pad_val=0, seg_pad_val=255),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg'])
+]
+valid_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(512, 512),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(
+                type='Normalize',
+                mean=[123.675, 116.28, 103.53],
+                std=[58.395, 57.12, 57.375],
+                to_rgb=True),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img'])
+        ])
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(512, 512),
+        img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5],
+        flip=True,
+        flip_direction=['horizontal', 'vertical'],
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(
+                type='Normalize',
+                mean=[123.675, 116.28, 103.53],
+                std=[58.395, 57.12, 57.375],
+                to_rgb=True),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img'])
+        ])
+]
+data = dict(
+    samples_per_gpu=8,
+    workers_per_gpu=2,
+    train=dict(
+        type='MultiImageMixDataset',
+        dataset=dict(
+            type='CustomDataset',
+            classes=[
+                'Background', 'General trash', 'Paper', 'Paper pack', 'Metal',
+                'Glass', 'Plastic', 'Styrofoam', 'Plastic bag', 'Battery',
+                'Clothing'
+            ],
+            palette=[[0, 0, 0], [192, 0, 128], [0, 128, 192], [0, 128, 64],
+                     [128, 0, 0], [64, 0, 128], [64, 0, 192], [192, 128, 64],
+                     [192, 192, 128], [64, 64, 128], [128, 0, 192]],
+            reduce_zero_label=False,
+            img_dir='/opt/ml/input/mmseg/images/train',
+            ann_dir='/opt/ml/input/mmseg/annotations/train',
+            pipeline=[
+                dict(type='LoadImageFromFile'),
+                dict(type='LoadAnnotations')
+            ]),
+        pipeline=[
+            dict(type='RandomMosaic', prob=0.5, img_scale=(512, 512)),
+            dict(type='Resize', img_scale=(512, 512), ratio_range=(0.5, 2.0)),
+            dict(type='RandomCrop', crop_size=(512, 512), cat_max_ratio=0.75),
+            dict(type='CLAHE'),
+            dict(type='RandomFlip', prob=0.5),
+            dict(type='PhotoMetricDistortion'),
+            dict(
+                type='Normalize',
+                mean=[123.675, 116.28, 103.53],
+                std=[58.395, 57.12, 57.375],
+                to_rgb=True),
+            dict(type='Pad', size=(512, 512), pad_val=0, seg_pad_val=255),
+            dict(type='DefaultFormatBundle'),
+            dict(type='Collect', keys=['img', 'gt_semantic_seg'])
+        ]),
+    val=dict(
+        classes=[
+            'Background', 'General trash', 'Paper', 'Paper pack', 'Metal',
+            'Glass', 'Plastic', 'Styrofoam', 'Plastic bag', 'Battery',
+            'Clothing'
+        ],
+        palette=[[0, 0, 0], [192, 0, 128], [0, 128, 192], [0, 128, 64],
+                 [128, 0, 0], [64, 0, 128], [64, 0, 192], [192, 128, 64],
+                 [192, 192, 128], [64, 64, 128], [128, 0, 192]],
+        type='CustomDataset',
+        reduce_zero_label=False,
+        img_dir='/opt/ml/input/mmseg/images/valid',
+        ann_dir='/opt/ml/input/mmseg/annotations/valid',
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(
+                type='MultiScaleFlipAug',
+                img_scale=(512, 512),
+                flip=False,
+                transforms=[
+                    dict(type='Resize', keep_ratio=True),
+                    dict(type='RandomFlip'),
+                    dict(
+                        type='Normalize',
+                        mean=[123.675, 116.28, 103.53],
+                        std=[58.395, 57.12, 57.375],
+                        to_rgb=True),
+                    dict(type='ImageToTensor', keys=['img']),
+                    dict(type='Collect', keys=['img'])
+                ])
+        ]),
+    test=dict(
+        classes=[
+            'Background', 'General trash', 'Paper', 'Paper pack', 'Metal',
+            'Glass', 'Plastic', 'Styrofoam', 'Plastic bag', 'Battery',
+            'Clothing'
+        ],
+        palette=[[0, 0, 0], [192, 0, 128], [0, 128, 192], [0, 128, 64],
+                 [128, 0, 0], [64, 0, 128], [64, 0, 192], [192, 128, 64],
+                 [192, 192, 128], [64, 64, 128], [128, 0, 192]],
+        type='CustomDataset',
+        reduce_zero_label=False,
+        img_dir='/opt/ml/input/mmseg/images/test',
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(
+                type='MultiScaleFlipAug',
+                img_scale=(512, 512),
+                img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5],
+                flip=True,
+                flip_direction=['horizontal', 'vertical'],
+                transforms=[
+                    dict(type='Resize', keep_ratio=True),
+                    dict(type='RandomFlip'),
+                    dict(
+                        type='Normalize',
+                        mean=[123.675, 116.28, 103.53],
+                        std=[58.395, 57.12, 57.375],
+                        to_rgb=True),
+                    dict(type='ImageToTensor', keys=['img']),
+                    dict(type='Collect', keys=['img'])
+                ])
+        ]))
+log_config = dict(
+    interval=50, hooks=[dict(type='TextLoggerHook', by_epoch=True)])
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
+cudnn_benchmark = True
+optimizer = dict(
+    type='AdamW',
+    lr=6e-05,
+    betas=(0.9, 0.999),
+    weight_decay=0.01,
+    paramwise_cfg=dict(
+        custom_keys=dict(
+            absolute_pos_embed=dict(decay_mult=0.0),
+            relative_position_bias_table=dict(decay_mult=0.0),
+            norm=dict(decay_mult=0.0))))
+optimizer_config = dict()
+lr_config = dict(
+    policy='poly',
+    warmup='linear',
+    warmup_iters=1500,
+    warmup_ratio=1e-06,
+    power=1.0,
+    min_lr=0.0,
+    by_epoch=True)
+runner = dict(type='EpochBasedRunner', max_epochs=40)
+checkpoint_config = dict(by_epoch=True, interval=50)
+evaluation = dict(metric='mIoU', pre_eval=True, save_best='mIoU')
+work_dir = './work_dirs/deeplabv3plus_r50-d8_512x512_80k_potsdam'
+gpu_ids = [0]
+auto_resume = False
